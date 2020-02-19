582bb9d15901f13cf7bd9dbe9aff8c099adda63f
Korina Pluss <korina.pluss@gmail.com>
2020-02-19 17:05:41 -0500

    check first word of commit message

--- src/commands/message/index.js
+++ src/commands/message/index.js
@@ -3,10 +3,13 @@ import fs from 'fs'
 const commitMsg = () => {
   try {
     console.log(process.argv)
-    const fd = fs.readFileSync(process.argv[3])
-    console.log(fd)
+    const commitMsg = fs.readFileSync(process.argv[3], 'utf8')
+    const firstWord = commitMsg.split(' ')[0]
+    if (!fistWord.startsWith(':') || !firstWord.endsWith(':')) {
+      throw new Error()
+    }
   } catch (error) {
-    console.error(error)
+    console.log('Commit message does not begin with an emoji')
     process.exit(1)
   } finally {
     process.exit(0)
