998ae26e1438cb448f1b475164b86f42a371d7c3
Korina Pluss <korina.pluss@gmail.com>
2020-02-19 17:12:10 -0500

    :art: testing commit hook msg

--- src/commands/hook/msg-hook.js
+++ src/commands/hook/msg-hook.js
@@ -4,11 +4,8 @@ const MSG_HOOK: Object = {
   PATH: '/hooks/commit-msg',
   CONTENTS: `#!/bin/sh
 # gitmoji as a commit hook
-if test -t 1 && ! grep -q -m 1 '^[^#]' $1; then
-  # it has been invoked from a tty and no commit message is already set
-  exec < /dev/tty
-  gitmoji --message $1
-fi
+exec < /dev/tty
+gitmoji --message $1
 `
 }
 
--- src/commands/message/index.js
+++ src/commands/message/index.js
@@ -2,14 +2,15 @@ import fs from 'fs'
 
 const commitMsg = () => {
   try {
-    console.log(process.argv)
     const commitMsg = fs.readFileSync(process.argv[3], 'utf8')
     const firstWord = commitMsg.split(' ')[0]
     if (!fistWord.startsWith(':') || !firstWord.endsWith(':')) {
       throw new Error()
     }
   } catch (error) {
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     console.log('Commit message does not begin with an emoji')
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     process.exit(1)
   } finally {
     process.exit(0)
